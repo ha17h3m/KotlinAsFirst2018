5222fbfe216c2b274584d1fecbac121d0067d44e
haithem <bekakra@inbox.ru>
2018-09-30 17:42:49 +0300

    l

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -146,6 +146,6 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = pow(1 + percent * 
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number%10)*100+(((number/10)%10)*10)+number/100
+fun numberRevert(number: Int): Int = (number% 10)*100+(((number/10)%10)*10)+number/100
 
 
